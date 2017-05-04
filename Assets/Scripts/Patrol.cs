f147ba734860e61d44ac318265f0b9ee2aae9245
WinGoodyGoody <stevencameronaustralia@gmail.com>
2017-05-04 15:41:48 +1000

    Project1 uploading

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrol : MonoBehaviour {
+
+	public Transform[] patrolPoints;
+	public float moveSpeed;
+
+	private int currentPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+		transform.position = patrolPoints [0].position;
+		currentPoint = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentPoint >= patrolPoints.Length)
+		{
+			currentPoint = 0;
+		}
+
+
+		if (transform.position == patrolPoints [currentPoint].position) 
+		{
+			currentPoint++;
+		}
+		//walk the character
+		transform.position=Vector3.MoveTowards(transform.position,patrolPoints[currentPoint].position,moveSpeed*Time.deltaTime);
+	
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	//Movement speed 
+	public float moveSpeed;
+
+	public GameObject deathParticles;
+
+	private float maxSpeed = 5f;
+
+	private Vector3 spawn;
+
+
+	//Location in 3D space
+	private Vector3 input;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Recording the start position of the player as the spawn position
+		// so that can return to here after a collision with the enemy
+		spawn = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+
+		if (GetComponent<Rigidbody> ().velocity.magnitude < maxSpeed) 
+		{
+			//print (input);
+
+			GetComponent<Rigidbody> ().AddForce (input * moveSpeed);
+		}
+
+		if (transform.position.y < -2)
+		{
+			Die ();
+		}
+			
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.transform.tag=="Enemy")
+		{
+			Die ();
+		}
+	}
+
+	void Die()
+	{
+		Instantiate (deathParticles, transform.position, Quaternion.identity);
+		transform.position = spawn;
+	}
+		
+}
